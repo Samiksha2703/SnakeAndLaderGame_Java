bf3d387280f1f979a7a97e7f63b3bf86a10873df
Samiksha2703 <shende.samiksha@rediffmail.com>
2021-03-10 15:06:09 +0530

    [Samiksha].ADD code to initialize player starting position

---
+++ SnakeAndLadder.java
@@ -0,0 +1,11 @@
+public class SnakeAndLadder {
+
+	// Declaring variables
+	public static final int initialPosition = 0;
+	public static final int finalPosition = 100;
+
+	// Main method to execute program
+	public static void main(String args[]) {
+		int playerPosition = initialPosition;
+	}
+}
