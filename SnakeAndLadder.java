ce73e32022b567b34ee387f3ea7114ec8e27123f
Samiksha2703 <shende.samiksha@rediffmail.com>
2021-03-10 15:30:17 +0530

    [Samiksha].ADD code to check No play, ladder or snake when player throw the die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,9 +6,31 @@ public class SnakeAndLadder {
 
 	// Main method to execute program
 	public static void main(String args[]) {
+		int dieNumber;
+		int currentPosition;
 		int playerPosition = initialPosition;
+
 		//Generating random number
-		int dieNumber = (int) Math.floor(Math.random() * 6) + 1;
-		playerPosition += dieNumber;
+		int playNoPlay = (int) Math.floor(Math.random() * 10) % 3;
+		currentPosition = playerPosition;
+		switch (playNoPlay) {
+		case 0:
+			playerPosition = currentPosition;
+			break;
+		case 1:
+			dieNumber = (int) Math.floor(Math.random() * 6) + 1;
+			playerPosition += dieNumber;
+			if (playerPosition > 100) {
+				playerPosition = currentPosition;
+			}
+			break;
+		case 2:
+			dieNumber = (int) Math.floor(Math.random() * 6) + 1;
+			playerPosition -= dieNumber;
+			if (playerPosition <= 1) {
+				playerPosition = 1;
+			}
+			break;
+		}
 	}
 }
